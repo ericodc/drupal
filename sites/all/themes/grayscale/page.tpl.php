de09e0758486bf4a2fb280e713ea82e31d04c677
erico <ericodc@gmail.com>
2013-09-06 00:58:03 -0300

    butabdo variaveis em grayscale

--- sites/all/themes/grayscale/page.tpl.php
+++ sites/all/themes/grayscale/page.tpl.php
@@ -1,26 +1,29 @@
 <html>
     <head>
-        <title>Page title</title>
+    <title><?php print $head_title ?></title>
         <link rel="stylesheet" href="global.css" type="text/css" />
     </head>
 <body>
     <div id="container">
         <div id="header">
-            <h1>Cabeçalho</h1>
+            <h1><?php print $site_name ?></h1>
+            <?php print $header ?>
         </div>
+        <?php if ($left) : ?>
         <div id="left">
-            <p>lorem ipsumsd sdhsjdh ks dksdfadahgdkahgd asdasdhgahs dah sdfjhagsdk adhf akjhdskajsd ja djhgasd haskdh asdajsd jasdk a sdj
-            </p>
+             <?php print $left ?>
         </div>
+            <?php endif; ?>
         <div id="main">
-            <h2>Subcabeçalho, pode ser um titulo</h2>
-            <p>
-              dasdf akdf asdfva dhgf adghf ajdajksd fjahs dfjah sdfjha sdfjkh asdkjfhaksdhf kasdh fkahs dfkjaghs qwe u wep ejrgnsbxv masvdkjagew 7ty sw fexcb vshdjk ge0r terg ksdnbf vjksh uywer tskdfn szndbfusye ptir e4=0uy erjh sueiry0 8745 ti
-            </p>
+            <?php print $breadcrumb ?>
+            <h2><?php print $title ?></h2>
+            <?php print $content ?>
         </div>
         <div id="footer">
-            footer
+            <?php print $footer_message ?>
+            <?php print $footer ?>
         </div>
     </div>
+<?php print $closure ?>
 </body>
 </html>
